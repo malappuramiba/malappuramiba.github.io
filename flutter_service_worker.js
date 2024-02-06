ad4110014ea307f8f3a22893a88d918c39d2e21a
malappuramiba <158708934+malappuramiba@users.noreply.github.com>
2024-02-06 16:01:18 +0530

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,9 +5,9 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "f3e25fa229c01f4c820fb6beb2d311fb",
 "favicon.ico": "2514772bdcb8cb5a97848db31225f22d",
-"index.html": "125003b5614e590e3b08c96a54b514f7",
-"/": "125003b5614e590e3b08c96a54b514f7",
-"main.dart.js": "cd35c0418370b932cc95e58f18945221",
+"index.html": "80cdb790d1796f47c564608a6a87df32",
+"/": "80cdb790d1796f47c564608a6a87df32",
+"main.dart.js": "83351c2e769ffa94388a2b98eedee675",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "icons/apple-touch-icon.png": "1b1e0bc604bca223f1e225409bae075a",
 "icons/icon-192.png": "1fb415c4d0c36d95e2bec9abb1d4ad2b",
